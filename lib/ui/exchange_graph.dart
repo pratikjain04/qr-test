5f09f0d7e37646eac4aa8ab8cf551196ba1af706
pratikjain04 <pratikjain0411@gmail.com>
2020-10-22 21:57:41 +0530

    working graph code added
    Signed-off-by: pratikjain04 <pratikjain0411@gmail.com>

--- lib/ui/exchange_graph.dart
+++ lib/ui/exchange_graph.dart
@@ -7,19 +7,21 @@ import 'package:qrtest/models/county_code_model.dart';
 import 'package:qrtest/services/download_excel.dart';
 import 'package:qrtest/utils/constants.dart';
 
-
 class ExchangeGraph extends StatefulWidget {
   @override
   _ExchangeGraphState createState() => _ExchangeGraphState();
 }
 
 class _ExchangeGraphState extends State<ExchangeGraph> {
-
+  //Circular loader detector
+  bool isLoading = false;
   List<charts.Series> seriesList = []; // for bar graph
   static List<String> outgoingCountries = []; // for outgoingCountryIDs
-  List<CountryCodeModel> countryCodeModel = []; // for storing country codes from data.json
+  List<CountryCodeModel> countryCodeModel =
+  []; // for storing country codes from data.json
   static List<dynamic> dataList = []; // for the dashboard data
-  static Map<String, int> graphData = Map<String,int>(); // for creating the final map of data
+  static Map<String, int> graphData =
+  Map<String, int>(); // for creating the final map of data
   /// graphData = {
   ///  "Argentina" : "2",
   ///  "Turkey" : "1"
@@ -28,30 +30,35 @@ class _ExchangeGraphState extends State<ExchangeGraph> {
   /// Use this graph to build List of ExchangeData which can be used
   /// in displaying the graph as done in _getCountryData()
 
-  // todo: call in initState after dataloading
   static List<charts.Series<ExchangeData, String>> _getCountryData() {
     final random = Random();
 
     // todo: build this data in initstate
-    var data = [
-      ExchangeData('Germany', random.nextInt(100)),
-      ExchangeData('Austria', random.nextInt(100)),
-      ExchangeData('Spain', random.nextInt(100)),
-      ExchangeData('Turkey', random.nextInt(100)),
-      ExchangeData('USA', random.nextInt(100)),
-      ExchangeData('SYRIA', random.nextInt(100)),
-      ExchangeData('Iraq', random.nextInt(100)),
-
+    List<ExchangeData> data = [
+      // ExchangeData('Germany', random.nextInt(100)),
+      // ExchangeData('Austria', random.nextInt(100)),
+      // ExchangeData('Spain', random.nextInt(100)),
+      // ExchangeData('Turkey', random.nextInt(100)),
+      // ExchangeData('USA', random.nextInt(100)),
+      // ExchangeData('SYRIA', random.nextInt(100)),
+      // ExchangeData('Iraq', random.nextInt(100)),
     ];
 
+    graphData.forEach((key, value) {
+      data.add(ExchangeData(key, value));
+    });
+    data.forEach((element) {
+      print("----");
+      print(element);
+      print("----");
+    });
     return [
       charts.Series<ExchangeData, String>(
-        id: 'Sales',
-        domainFn: (ExchangeData data, _) => data.countryName,
-        measureFn: (ExchangeData data, _) => data.offersExchanged,
-        data: data,
-        displayName: "Your Exchange Data Per Country"
-      )
+          id: 'Sales',
+          domainFn: (ExchangeData data, _) => data.countryName,
+          measureFn: (ExchangeData data, _) => data.offersExchanged,
+          data: data,
+          displayName: "Your Exchange Data Per Country")
     ];
   }
 
@@ -66,8 +73,9 @@ class _ExchangeGraphState extends State<ExchangeGraph> {
 
   /// building CountryCodes List from data.json
   /// to access use [countryModel[i].country_code]
- Future<void> buildCountryCodeList() async {
-    String data = await DefaultAssetBundle.of(context).loadString("assets/data.json");
+  Future<void> buildCountryCodeList() async {
+    String data =
+    await DefaultAssetBundle.of(context).loadString("assets/data.json");
     List<dynamic> res = json.decode(data)["code"];
 
     countryCodeModel?.clear();
@@ -75,38 +83,46 @@ class _ExchangeGraphState extends State<ExchangeGraph> {
     res.forEach((data) {
       countryCodeModel.add(CountryCodeModel.fromJson(data));
     });
-
   }
 
-
- Future<void> calc() async {
+  Future<void> calc() async {
+    setState(() {
+      isLoading = true;
+    });
     dataList?.clear();
-    dataList = DownloadService.offerList.toSet().toList(); // list of scanned data from firebase
+    dataList = DownloadService.offerList
+        .toSet()
+        .toList(); // list of scanned data from firebase
     outgoingCountries?.clear();
+
     /// adding outgoingID to a list for grouping it later
     dataList.forEach((element) {
-      outgoingCountries.add(element["Outgoing_ID"].toString().substring(0,2));
+      outgoingCountries.add(element["Outgoing_ID"].toString().substring(0, 2));
     });
+
     ///sample output for outgoingCountries = ['AR', 'AR', 'TR'];
 
     outgoingCountries.forEach((id) {
-      // todo: count number of offers from each country India has exchanged with
       /// Argentina : 2, Turkey: 1 and display this data on bar graph
       print(id);
       graphData[id] = graphData[id] == null ? 1 : graphData[id] + 1;
     });
     print(graphData); // this graph has all the required information
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
   void initState() {
     super.initState();
 
-    buildCountryCodeList().then((value){
-      calc();
+    buildCountryCodeList().then((value) {
+      calc().then((value) {
+        seriesList = _getCountryData();
+      });
     });
     seriesList = _getCountryData();
-
   }
 
   @override
@@ -116,9 +132,8 @@ class _ExchangeGraphState extends State<ExchangeGraph> {
         body: SafeArea(
           child: SingleChildScrollView(
             child: Container(
-              height: Constants.height/2.25,
-              child: barChart()
-            ),
+                height: Constants.height / 2.25,
+                child: isLoading ? CircularProgressIndicator() : barChart()),
           ),
         ),
       ),
@@ -126,13 +141,10 @@ class _ExchangeGraphState extends State<ExchangeGraph> {
   }
 }
 
-
 // model class for Exchange Data
 class ExchangeData {
-
   final String countryName;
   final int offersExchanged;
 
   ExchangeData(this.countryName, this.offersExchanged);
-
 }
